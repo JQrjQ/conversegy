d1f844f3dfef4ef5b0f58c58eef4346b64ff4946
John Quinn <john.quinn.cert@gmail.com>
2022-11-12 11:47:14 +0000

    fixing homepage layout

--- src/static/js/main.js
+++ src/static/js/main.js
@@ -1,174 +1,72 @@
-var canvas = document.getElementById("canvas"),
-    ctx = canvas.getContext('2d');
-
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
-
-var stars = [], // Array that contains the stars
-    FPS = 60, // Frames per second
-    x = 100, // Number of stars
-    mouse = {
-      x: 0,
-      y: 0
-    };  // mouse location
-
-// Push stars to array
-
-for (var i = 0; i < x; i++) {
-  stars.push({
-    x: Math.random() * canvas.width,
-    y: Math.random() * canvas.height,
-    radius: Math.random() * 1 + 1,
-    vx: Math.floor(Math.random() * 50) - 25,
-    vy: Math.floor(Math.random() * 50) - 25
-  });
-}
-
-// Draw the scene
-
-function draw() {
-  ctx.clearRect(0,0,canvas.width,canvas.height);
-  
-  ctx.globalCompositeOperation = "lighter";
-  
-  for (var i = 0, x = stars.length; i < x; i++) {
-    var s = stars[i];
-  
-    ctx.fillStyle = "#fff";
-    ctx.beginPath();
-    ctx.arc(s.x, s.y, s.radius, 0, 2 * Math.PI);
-    ctx.fill();
-    ctx.fillStyle = 'black';
-    ctx.stroke();
-  }
-  
-  ctx.beginPath();
-  for (var i = 0, x = stars.length; i < x; i++) {
-    var starI = stars[i];
-    ctx.moveTo(starI.x,starI.y); 
-    if(distance(mouse, starI) < 150) ctx.lineTo(mouse.x, mouse.y);
-    for (var j = 0, x = stars.length; j < x; j++) {
-      var starII = stars[j];
-      if(distance(starI, starII) < 150) {
-        //ctx.globalAlpha = (1 / 150 * distance(starI, starII).toFixed(1));
-        ctx.lineTo(starII.x,starII.y); 
-      }
-    }
-  }
-  ctx.lineWidth = 0.05;
-  ctx.strokeStyle = 'white';
-  ctx.stroke();
-}
-
-function distance( point1, point2 ){
-  var xs = 0;
-  var ys = 0;
- 
-  xs = point2.x - point1.x;
-  xs = xs * xs;
- 
-  ys = point2.y - point1.y;
-  ys = ys * ys;
- 
-  return Math.sqrt( xs + ys );
-}
-
-// Update star locations
-
-function update() {
-  for (var i = 0, x = stars.length; i < x; i++) {
-    var s = stars[i];
-  
-    s.x += s.vx / FPS;
-    s.y += s.vy / FPS;
-    
-    if (s.x < 0 || s.x > canvas.width) s.vx = -s.vx;
-    if (s.y < 0 || s.y > canvas.height) s.vy = -s.vy;
-  }
-}
-
-canvas.addEventListener('mousemove', function(e){
-  mouse.x = e.clientX;
-  mouse.y = e.clientY;
-});
-
-// Update and draw
-
-function tick() {
-  draw();
-  update();
-  requestAnimationFrame(tick);
-}
-
-tick(); 
-
-// Typewriter
+//Typewriter
 class TypeWriter {
-    constructor(txtElement, words, wait = 3000) {
-      this.txtElement = txtElement;
-      this.words = words;
-      this.txt = '';
-      this.wordIndex = 0;
-      this.wait = parseInt(wait, 10);
-      this.type();
-      this.isDeleting = false;
-    }
-  
-    type() {
-      // Current index of word
-      const current = this.wordIndex % this.words.length;
-      // Get full text of current word
-      const fullTxt = this.words[current];
-  
-      // Check if deleting
-      if(this.isDeleting) {
-        // Remove char
-        this.txt = fullTxt.substring(0, this.txt.length - 1);
-      } else {
-        // Add char
-        this.txt = fullTxt.substring(0, this.txt.length + 1);
-      }
-  
-      // Insert txt into element
-      this.txtElement.innerHTML = `<span class="txt">${this.txt}</span>`;
-  
-      // Initial Type Speed
-      let typeSpeed = 300;
-  
-      if(this.isDeleting) {
-        typeSpeed /= 2;
-      }
-  
-      // If word is complete
-      if(!this.isDeleting && this.txt === fullTxt) {
-        // Make pause at end
-        typeSpeed = this.wait;
-        // Set delete to true
-        this.isDeleting = true;
-      } else if(this.isDeleting && this.txt === '') {
-        this.isDeleting = false;
-        // Move to next word
-        this.wordIndex++;
-        // Pause before start typing
-        typeSpeed = 500;
-      }
-  
-      setTimeout(() => this.type(), typeSpeed);
-    }
-  }
-  
-  
-  // Init On DOM Load
-  document.addEventListener('DOMContentLoaded', init);
-  
-  // Init App
-  function init() {
-    const txtElement = document.querySelector('.txt-type');
-    const words = JSON.parse(txtElement.getAttribute('data-words'));
-    const wait = txtElement.getAttribute('data-wait');
-    // Init TypeWriter
-    new TypeWriter(txtElement, words, wait);
+  constructor(txtElement, words, wait = 3000) {
+    this.txtElement = txtElement;
+    this.words = words;
+    this.txt = '';
+    this.wordIndex = 0;
+    this.wait = parseInt(wait, 10);
+    this.type();
+    this.isDeleting = false;
   }
 
-  //End Typewriter
+  type() {
+    // Current index of word
+    const current = this.wordIndex % this.words.length;
+    // Get full text of current word
+    const fullTxt = this.words[current];
+
+    // Check if deleting
+    if(this.isDeleting) {
+      // Remove char
+      this.txt = fullTxt.substring(0, this.txt.length - 1);
+    } else {
+      // Add char
+      this.txt = fullTxt.substring(0, this.txt.length + 1);
+    }
+
+    // Insert txt into element
+    this.txtElement.innerHTML = `<span class="txt">${this.txt}</span>`;
+
+    // Initial Type Speed
+    let typeSpeed = 300;
+
+    if(this.isDeleting) {
+      typeSpeed /= 2;
+    }
+
+    // If word is complete
+    if(!this.isDeleting && this.txt === fullTxt) {
+      // Make pause at end
+      typeSpeed = this.wait;
+      // Set delete to true
+      this.isDeleting = true;
+    } else if(this.isDeleting && this.txt === '') {
+      this.isDeleting = false;
+      // Move to next word
+      this.wordIndex++;
+      // Pause before start typing
+      typeSpeed = 500;
+    }
+
+    setTimeout(() => this.type(), typeSpeed);
+  }
+}
+
+
+// Init On DOM Load
+document.addEventListener('DOMContentLoaded', init);
+
+// Init App
+function init() {
+  const txtElement = document.querySelector('.txt-type');
+  const words = JSON.parse(txtElement.getAttribute('data-words'));
+  const wait = txtElement.getAttribute('data-wait');
+  // Init TypeWriter
+  new TypeWriter(txtElement, words, wait);
+}
+
+//End Typewriter
+
+
 
