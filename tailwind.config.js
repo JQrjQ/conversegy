808f63a3132ec370a1f15d2ed3419a1ca4a615c4
John Quinn <john.quinn.cert@gmail.com>
2022-11-15 12:27:54 +0000

    Finsished recent work section

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,14 +10,11 @@ module.exports = {
       lg: '1024px',
       xl: '1200px',
     },
-   /* fontFamily: {
-      'sans': ['ui-sans-serif', 'system-ui'],
-      'serif': ['ui-serif', 'Georgia'],
-      'mono': ['ui-monospace', 'SFMono-Regular'],
-      'display': ['Oswald'],
-      'body': ['"Open Sans"'],
-    },*/
-    
+    fontFamily: {
+      'lato': ['Lato'],
+      'mono': ['Roboto Mono', 'monospace'],
+    },
+  
     colors: {
       'blackish': '#5F6366',
       'blueish': '#301008',
